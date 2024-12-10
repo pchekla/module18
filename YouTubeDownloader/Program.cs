a698561c7c926e66e1163bea846aa198bdbab38c
v.sedlachek <v.sedlachek@dodobrands.io>
2024-12-10 07:04:20 +0300

    First one commit

---
+++ YouTubeDownloader/Program.cs
@@ -0,0 +1,13 @@
+namespace YouTubeDownloader;
+
+/// <summary>
+/// Точка входа в приложение.
+/// </summary>
+/// <param name="args">Аргументы командной строки.</param>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+    }
+}
